8a98d52b5d5cbaaa017b77a71285485686648665
Josh Levy <joshlevy89@gmail.com>
2016-04-14 09:01:26 -0400

    added redux store and middleware to app

--- app/App.js
+++ app/App.js
@@ -1,9 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { createStore, applyMiddleware } from 'redux'
+import logger from 'redux-logger'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import reducers from './reducers'
+
+
+var isProduction = process.env.NODE_ENV === 'production';
+
+const middleware = isProduction ? [ thunk ]:[thunk, logger()]
+let store = createStore(
+	applyMiddleware(...middleware)
+)
+
 
 export default class App extends Component {
   render() {
     return (
-      <h1>Hello, worl.</h1>
+      <Provider store={store}>
+      	<h1>Hello, world!</h1>
+      </Provider>
     );
   }
 }
