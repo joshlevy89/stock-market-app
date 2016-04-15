00366eb270ba7e79408e4282a2fdf28acf466959
Josh Levy <joshlevy89@gmail.com>
2016-04-15 19:44:24 -0400

    took out plugins

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -22,14 +22,7 @@ var config = {
       test: /\.css$/,
       loader: 'style!css'
     }]
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    })
-  ]
+  }
 };
 
 module.exports = config;
