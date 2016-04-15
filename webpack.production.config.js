573d19127cf62b94ee04490745abd6bb03087d88
Josh Levy <joshlevy89@gmail.com>
2016-04-15 19:00:40 -0400

    updated to include uglify and env production variable

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -23,6 +23,18 @@ var config = {
       loader: 'style!css'
     }]
   }
+  plugins:[
+  new webpack.DefinePlugin({
+    'process.env':{
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress:{
+      warnings: true
+    }
+  })
+]
 };
 
 module.exports = config;
