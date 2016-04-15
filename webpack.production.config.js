6b9177b8621f62e5c2f2325de18ff4a933575e75
Josh Levy <joshlevy89@gmail.com>
2016-04-15 19:02:58 -0400

    changed back

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -23,18 +23,6 @@ var config = {
       loader: 'style!css'
     }]
   }
-  plugins:[
-  new webpack.DefinePlugin({
-    'process.env':{
-      'NODE_ENV': JSON.stringify('production')
-    }
-  }),
-  new webpack.optimize.UglifyJsPlugin({
-    compress:{
-      warnings: true
-    }
-  })
-]
 };
 
 module.exports = config;
