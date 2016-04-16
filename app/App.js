c09de840485950d7d68a2648a8a531d4c2eaa9d8
Josh Levy <joshlevy89@gmail.com>
2016-04-16 15:00:25 -0400

    added reducer to store

--- app/App.js
+++ app/App.js
@@ -4,21 +4,21 @@ import logger from 'redux-logger'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import reducers from './reducers'
-
+import StockGraph from './containers/StockGraph'
 
 var isProduction = process.env.NODE_ENV === 'production';
 
 const middleware = isProduction ? [ thunk ]:[thunk, logger()]
 let store = createStore(
+	reducers,
 	applyMiddleware(...middleware)
 )
-
 
 export default class App extends Component {
   render() {
     return (
       <Provider store={store}>
-      	<h1>Hello, world!</h1>
+      	<StockGraph />
       </Provider>
     );
   }
