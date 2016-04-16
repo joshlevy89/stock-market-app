102c57f003747fdc40cba80815319e26a216f77c
Josh Levy <joshlevy89@gmail.com>
2016-04-16 15:02:11 -0400

    added reducer to receive a new stock

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,3 +1,10 @@
-export function temporary(state=[],action) {
-	return state
+export default function reducer(state=[],action) {
+	switch (action.type) {
+		case 'RECEIVE_STOCK':
+			return Object.assign({},state,{
+				[action.stockId]:action.dataset
+			})
+		default:
+			return state
+	}
 }
