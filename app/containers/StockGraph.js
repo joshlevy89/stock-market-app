2abeacb95e118bff36ae1852ee1b19d2bf06edf4
Josh Levy <joshlevy89@gmail.com>
2016-04-21 23:03:11 -0400

    enabled multiple stocks to be plotted

--- app/containers/StockGraph.js
+++ app/containers/StockGraph.js
@@ -1,47 +1,92 @@
 import React, { Component } from 'react'
-import { quandle_request } from '../actions'
-var BarChart = require("react-chartjs").Line
+import { quandle_request, delete_stock } from '../actions'
+var LineChart = require("react-chartjs").Line
 import { connect } from 'react-redux'
+var moment = require('moment');
+import { chartOptions, emptyChart, stockDatasetSkeleton } from '../helpers/chartParams.js'
+import StockList from '../components/StockList'
 
 class StockGraph extends Component {
-  render() {
-  const { quandle_request } = this.props
 
+  handleInputChange(e,val) {
+  	const { quandle_request } = this.props
+  	if (e.keyCode === 13) {
+  		quandle_request(val)
+  	}
+  }
+
+  render() {
+	const { stocks, delete_stock } = this.props
+
+	// get the utc for the farthest date in the past
+  	var numDays = 365;
+  	var lowestUtc = moment().subtract(numDays,"days").valueOf();
+
+  	// initialize chart data
 	var chartData = {
-	    labels: ["January", "February", "March", "April", "May", "June", "July"],
-	    datasets: [
-	        {
-	            label: "My First dataset",
-	            fillColor: "rgba(220,220,220,0.2)",
-	            strokeColor: "rgba(220,220,220,1)",
-	            pointColor: "rgba(220,220,220,1)",
-	            pointStrokeColor: "#fff",
-	            pointHighlightFill: "#fff",
-	            pointHighlightStroke: "rgba(220,220,220,1)",
-	            data: [65, 59, 80, 81, 56, 55, 40]
-	        }
-	    ]
+	    labels: [],
+	    datasets: []
 	};
+
+	// if no stocks, make an empty placeholder chart
+	if (Object.keys(stocks).length === 0) { 
+		var foo = [];
+		var labels = [];
+		for (var i = 1; i <= numDays/10; i++) {
+			foo.push(i);
+			labels.push('');
+		}
+		chartData = emptyChart(foo,labels)
+	};
+
+   // for every stock, add it to the chartData datasets
+   for (var prop in stocks) {
+   	// get stock data
+   	var data = stocks[prop].data;
+   	// filter the data to date range, and reverse so that oldest to newest
+   	var keeperData = data.filter(d=>{
+   		return moment(d[0]).valueOf()>lowestUtc
+   		}).reverse();
+
+   	// return stock price and add to dataset
+   	var price = keeperData.map(d=>{return d[1]})
+   	var stockObj = stockDatasetSkeleton;
+   	stockObj.data = price;
+    var tObj = JSON.parse(JSON.stringify(stockObj))
+    chartData.datasets.push(tObj);
+
+    // set the labels (unecessarily happens every time)
+    var allLabels = keeperData.map(d=>{return d[0]})
+    var someLabels = [];
+    for (i = 0;i<allLabels.length;i++) { // sparsifies labels for readability
+    	if (i % 10 === 0) someLabels.push(allLabels[i])
+    	else someLabels.push('') 
+    }
+    chartData.labels = someLabels;
+   }
 
     return (
    	<div>
     <h1>Stock Graph</h1>
-    <BarChart data={chartData}/>
-    <button onClick = {() => quandle_request()}>Make Quandle Request</button>
+    <div><LineChart data={chartData} options={chartOptions} width="600" height="250" redraw/></div>
+    {/*<button onClick = {() => quandle_request()}>Make Quandle Request</button>*/}
+    Stock Symbol: <input ref="myInput" 
+    			  onKeyDown={e=>this.handleInputChange(e,this.refs.myInput.value)}/>
+    <StockList {...this.props}/>
     </div>
     );
   }
 }
 
-function mapStateToProps() {
+function mapStateToProps(state) {
 	return {
-
+		stocks: state.stocks
 	}
 }
 
 StockGraph = connect(
 mapStateToProps,
-{ quandle_request }
+{ quandle_request, delete_stock }
 )(StockGraph)
 
 export default StockGraph
