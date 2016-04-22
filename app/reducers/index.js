c5f9785535138ebbf4fe29fd239037869f2138a4
Josh Levy <joshlevy89@gmail.com>
2016-04-21 23:03:35 -0400

    included stock removal handling

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,10 +1,21 @@
-export default function reducer(state=[],action) {
+import { combineReducers } from 'redux'
+import { Map } from 'immutable'
+
+export function stocks(state={},action) {
 	switch (action.type) {
 		case 'RECEIVE_STOCK':
 			return Object.assign({},state,{
 				[action.stockId]:action.dataset
 			})
+		case 'DELETE_STOCK':
+		    var state_copy = JSON.parse(JSON.stringify(state))
+			delete state_copy[action.key];
+			return state_copy
 		default:
 			return state
 	}
 }
+
+export default combineReducers({
+	stocks
+})
